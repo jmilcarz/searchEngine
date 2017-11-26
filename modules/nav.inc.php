6ce02d51176501f1842ee756aebb30ac5136c0a4
kmilcarz <j.milcarz@yahoo.com>
2017-11-26 09:55:24 +0100

    nav design completed

---
+++ modules/nav.inc.php
@@ -0,0 +1,9 @@
+<nav id="sitenav">
+     <div class="container">
+          <a href="index.php">szukaj</a>
+          <form action="search.php" method="get">
+               <input type="text" name="q" placeholder="szukaj...">
+               <button type="submit">szukaj</button>
+          </form>
+     </div>
+</nav>
